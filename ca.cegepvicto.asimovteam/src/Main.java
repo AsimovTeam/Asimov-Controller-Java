aa53da3d000a97f635b689769245eb425f504e09
EntrybotJS <entrybotjs@gmail.com>
2018-02-13 14:49:52 -0500

    Now able to communicate with the server

--- ca.cegepvicto.asimovteam/src/Main.java
+++ ca.cegepvicto.asimovteam/src/Main.java
@@ -1,4 +1,7 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.*;
 
 public class Main {
@@ -14,11 +17,19 @@ public class Main {
         //Éssai de connexion au serveur
         try {
             socket = new Socket(host, port);
+
+            PrintWriter outputStream = new PrintWriter(socket.getOutputStream(), true);
+
+            outputStream.println("hello world!");
+            outputStream.println("close");
+
         } catch (Exception e) {
+
             if(e.getMessage().equals("Connection refused: connect")) {
                 System.out.println("Server is offline/unreachable");
                 System.exit(-1);
             }
+
             e.printStackTrace();
             System.exit(-1);
         }
